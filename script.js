111192c48fb869fd6628c4d434861366fe8454e3
CosminOstafie <116910376+CosminOstafie@users.noreply.github.com>
2023-04-12 01:14:23 +0200

    CSS Changes + localStorage update 1

--- script.js
+++ script.js
@@ -27,11 +27,41 @@ function addTask() {
   listElement.appendChild(newListItem);
   ItemsArray.push(objectItem);
 
+  // Store the updated ItemsArray in local storage
+  updateLocalStorage();
+
   //Add the button to remove the item
   const deleteBtn = document.createElement("button");
   deleteBtn.innerText = "X";
   deleteBtn.classList.add("deleteBtn");
   newListItem.appendChild(deleteBtn);
 
+  //Adapted from chatgpt lines 42->43
+  //Remove item from the screen and local storage when the delete button is clicked
+  deleteBtn.addEventListener("click", () => {
+    let itemIndex = ItemsArray.findIndex((item) => item.text === textInput);
+    ItemsArray.splice(itemIndex, 1);
+    localStorage.setItem("ItemsArray", JSON.stringify(ItemsArray));
+    newListItem.remove();
+  });
+
+  //Toggle completed state when an item is clicked
+  newListItem.addEventListener("click", function () {
+    if (objectItem.completed) {
+      objectItem.completed = false;
+      newListItem.classList.remove("completed");
+      updateLocalStorage();
+    } else {
+      objectItem.completed = true;
+      newListItem.classList.add("completed");
+      updateLocalStorage();
+    }
+  });
+
+  //Reset the input after adding a new item to the list
   inputBox.value = "";
+}
+
+function updateLocalStorage() {
+  localStorage.setItem("ItemsArray", JSON.stringify(ItemsArray));
 }
