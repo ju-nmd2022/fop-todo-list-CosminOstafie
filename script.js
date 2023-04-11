cc34a836e649e5123983f078e55cc8dd445dd068
CosminOstafie <116910376+CosminOstafie@users.noreply.github.com>
2023-04-11 02:03:59 +0200

    Update javascript

---
+++ script.js
@@ -0,0 +1,36 @@
+//Selecting the HTML elements
+
+const listElement = document.querySelector("#listElement");
+const textInput = document.querySelector("#textInput");
+const addItemBtn = document.querySelector("#addItem");
+const saveBtn = document.querySelector("#saveList");
+const removeItemBtn = document.querySelector("#removeItem");
+
+//Event Listener for the Add item button
+addItemBtn.addEventListener("click", addItem);
+
+//Function to add items to the list
+function addItem() {
+  //Get the input value
+  const itemText = textInput.value;
+
+  if (itemText === "") {
+    return;
+  }
+
+  //Create new list item and add it to the list
+  const newItem = document.createElement("li");
+  newItem.innerText = itemText;
+  listElement.appendChild(newItem);
+
+  //Add the button to remove the item
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "X";
+  deleteButton.classList.add("delete");
+  listElement.appendChild(deleteButton);
+
+  // Event listener for the delete button
+  deleteButton.addEventListener("click", () => {
+    listElement.remove();
+  });
+}
